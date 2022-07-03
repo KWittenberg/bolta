759467d868653f7cc8d45c494755c9473badf125
K. Wittenberg <kejo@net.hr>
2022-07-03 17:39:51 +0200

    Add project files.

---
+++ assets/js/knjige.js
@@ -0,0 +1,31 @@
+var cijenaGrobljaPozeskeDoline = 200;
+var cijenaRudina = 300;
+var cijenaPuvarija = 300;
+var cijenaSesvetackiKrajUSrcuPoljadije = 300;
+var cijenaPjesme = 80;
+var cijenaPutopisneImpresije = 100;
+var cijenaOsamGeneracijaThallera = 200;
+var cijenaVetovo = 300;
+var cijena100GodinaDVDBuk = 150;
+var cijenaAlmanahGimnazije = 200;
+var cijenaBrestovac = 300;
+var cijenaSmotra = 100;
+var cijenaTekic = 200;
+
+
+document.getElementById('cijenaGrobljaPozeskeDoline').innerHTML = cijenaGrobljaPozeskeDoline;
+document.getElementById('cijenaRudina').innerHTML = cijenaRudina;
+document.getElementById('cijenaPuvarija').innerHTML = cijenaPuvarija;
+document.getElementById('cijenaSesvetackiKrajUSrcuPoljadije').innerHTML = cijenaSesvetackiKrajUSrcuPoljadije;
+document.getElementById('cijenaPjesme').innerHTML = cijenaPjesme;
+document.getElementById('cijenaPutopisneImpresije').innerHTML = cijenaPutopisneImpresije;
+document.getElementById('cijenaOsamGeneracijaThallera').innerHTML = cijenaOsamGeneracijaThallera;
+document.getElementById('cijenaVetovo').innerHTML = cijenaVetovo;
+document.getElementById('cijena100GodinaDVDBuk').innerHTML = cijena100GodinaDVDBuk;
+document.getElementById('cijenaAlmanahGimnazije').innerHTML = cijenaAlmanahGimnazije;
+document.getElementById('cijenaBrestovac').innerHTML = cijenaBrestovac;
+document.getElementById('cijenaSmotra').innerHTML = cijenaSmotra;
+document.getElementById('cijenaTekic').innerHTML = cijenaTekic;
+
+
+//document.write(cijenaGrobljaPozeskeDoline);
